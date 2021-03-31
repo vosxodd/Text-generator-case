2c23405a8cbe8c9c7fd13852768292f9e4c7def4
vosxodd <73089079+vosxodd@users.noreply.github.com>
2021-03-31 10:25:25 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,24 +9,32 @@ text = input().replace('/n', ' ')
 def correct_text():
     """Удаление из текста символов кроме чисел, букв и разрешённых знаков препинания"""
     global text
-    chars = ('~', '`', '@', '#', '№', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '_', ':', ';', '{', '}', '[', ']', '/', '|', '<', '>', '\'', '\"', "—")
+    chars = (
+    '~', '`', '@', '#', '№', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '_', ':', ';', '{', '}', '[', ']', '/',
+    '|', '<', '>', '\'', '\"', "—")
     for letter in text:
         if letter in chars:
             text = text.replace(letter, '')
 
+
 correct_text()
-text=text.split(" ")
-unic=[]
-b=0
+print(text, 'после удаления ненужных символов')
+text = text.split(" ")
+for word in text:
+    if word == '':
+        text.remove(word)
+print(text, 'после сплита')
+unic = []
+b = 0
 for i in text:
     for j in unic:
-        if i==j:
-            b=1
-    if b!=1:
+        if i == j:
+            b = 1
+    if b != 1:
         unic.append(i)
-    b=0        
-big=[]
+    b = 0
+big = []
 for i in unic:
-    if i==i.capitalize():
+    if i == i.capitalize():
         big.append(i)
 print(big)
