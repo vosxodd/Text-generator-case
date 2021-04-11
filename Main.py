3b5c07269a0d4129569ab95682283d99e46f1b83
vosxodd <73089079+vosxodd@users.noreply.github.com>
2021-04-11 15:15:52 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@
 #               Soloveychik D. (%),
 #               Labuzov A. (%)
 import random
+
 text = input().replace('/n', ' ')
 
 
@@ -10,20 +11,22 @@ def correct_text():
     """Удаление из текста символов кроме чисел, букв и разрешённых знаков препинания"""
     global text
     chars = (
-    '~', '`', '@', '#', '№', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '_', ':', ';', '{', '}', '[', ']', '/',
-    '|', '<', '>', '\'', '\"', "—")
+        '~', '`', '@', '#', '№', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '_', ':', ';', '{', '}', '[', ']',
+        '/',
+        '|', '<', '>', '\'', '\"', "—")
     for letter in text:
         if letter in chars:
             text = text.replace(letter, '')
 
 
 correct_text()
-#print(text, 'после удаления ненужных символов')
+# print(text, 'после удаления ненужных символов')
 text = text.split(" ")
 for word in text:
     if word == '':
         text.remove(word)
-#print(text, 'после сплита')
+# print(text, 'после сплита')
+
 unic = []
 b = 0
 for i in text:
@@ -37,30 +40,30 @@ big = []
 for i in unic:
     if i == i.capitalize():
         big.append(i)
-nexty=[] #список списков
-dop="" #переменная для создания списка списков
+nexty = []  # список списков
 for i in unic:
-    dop=""
-    for j in range(len(text)):
-        if i==text[j] and j!=len(text)-1:
-            dop=dop+text[j+1]+" "
-    if dop!="":
-        dop=dop.split(" ")
+    dop = ""  # переменная для создания списка списков
+    for j in range(len(text) - 1):
+        if i == text[j]:
+            dop = dop + text[j + 1] + " "
+    if dop != "":
+        dop = dop.split(" ")
         for word in dop:
             if word == '':
                 dop.remove(word)
         nexty.append(dop)
-#print(unic)
-#print(nexty)
-#print(big)
-start=random.choice(big)#случайное слово с большой буквы
-helpy=""#вспомогательная переменная
-for i in range(0,len(text)):#версия где столько же слов сколько в оригинале
-    if start==unic[len(unic)-1]:
-        start=random.choice(big)
-    for j in range(0,len(unic)):
-        if start==unic[j] and j!=len(unic)-1:
-            helpy=helpy+start+" "
-            start=random.choice(nexty[j])
+#print(unic, 'список уникальных слов')
+#print(nexty, 'список звеньев и связок')
+#print(big, 'список слов с заглавной буквы')
+
+start = random.choice(big)  # случайное слово с большой буквы
+helpy = ""  # вспомогательная переменная
+for i in range(len(text)):  # версия, где столько же слов, сколько в оригинале
+    if start == unic[-1]:
+        start = random.choice(big)
+    for j in range(len(unic) - 1):
+        if start == unic[j]:
+            helpy = helpy + start + " "
+            start = random.choice(nexty[j])
             break
 print(helpy)
